f1b41b54af9588486fc0f78d7755b5c6b8b726da
edubel26 <beltran502@hotmail.com>
2024-06-21 20:52:44 -0500

    finalizacion de api

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ require('dotenv').config()
 const DB_CONNECTION = process.env.DB_CONNECTION || ''
 mongoose.connect(DB_CONNECTION)// creo la cadena de conexion
 
+const cors = require('cors') // Importo la libreria de cors 
+app.use(cors());// permite la entrada y salida de datos 
+
 // Importamos las rutas del otro archivo
 app.use(express.urlencoded({extended: true}))// Acceder a la informacion de las urls
 app.use(express.json())// Analizar informacion en formato json
